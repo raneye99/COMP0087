477256902c1d88d1fb6ffd7261a1503ee2af289a
raneye99 <eileen.raney@gmail.com>
2022-04-01 16:43:50 +0100

    add preprocessing script

--- mtl/preprocess.py
+++ mtl/preprocess.py
@@ -15,3 +15,26 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
 from sklearn.utils import shuffle
+
+def clean_text(data):
+    
+    # remove hashtags and @usernames
+    data = re.sub(r"(#[\d\w\.]+)", '', data)
+    data = re.sub(r"(@[\d\w\.]+)", '', data)
+    
+    # tekenization using nltk
+    data = word_tokenize(data)
+    
+    return data
+
+def create_embedding_matrix(filepath, word_index, embedding_dim):
+    vocab_size = len(word_index) + 1  # Adding again 1 because of reserved 0 index
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+    with open(filepath) as f:
+        for line in f:
+            word, *vector = line.split()
+            if word in word_index:
+                idx = word_index[word] 
+                embedding_matrix[idx] = np.array(
+                    vector, dtype=np.float32)[:embedding_dim]
+    return embedding_matrix
