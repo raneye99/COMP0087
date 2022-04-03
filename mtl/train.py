b1e3186960dd9a79e6f462aca920370255a33c7a
raneye99 <eileen.raney@gmail.com>
2022-04-03 18:49:19 +0100

    modified training to take params

--- mtl/train.py
+++ mtl/train.py
@@ -7,17 +7,13 @@ import datetime
 from tensorboard.plugins.hparams import api as hp
 
 from mtl.network import net
-from mtl.preprocess import clean_text, create_embedding_matrix, data_processing
-X_train, X_test, X_train_pad, X_test_pad, y_train, y_test, y_train1, y_test1, y_train2, y_test2, y_train3, y_test3, y_train4, y_test4, y_train5, y_test5, y_train6, y_test6, embedd_matrix, vocab_size, X_val, X_val_pad, y_val, y_val1, y_val2, y_val3, y_val4, y_val5, y_val6, data_test = data_processing()
 
-
-
-def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_size=128, epochs=1, gru_output_size=128, dropout=0.2, recurrent_dropout=0.2):
+def train_model(vocab_size, embedd_matrix, model_name, data_x, data_y, val_x, val_y, saved_model_name, tensorboard,  loss_weights, batch_size=128, epochs=1, gru_output_size=128, dropout=0.2, recurrent_dropout=0.2):
 
     print('Training a '+model_name+' model!')
 
     if model_name == 'mtl':
-        mod = net().mtl_model(gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout)
+        mod = net().mtl_model(vocab_size = vocab_size, embedd_matrix = embedd_matrix, gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout, num_classes = 7, num_classes1=2, embed_num_dims=300, max_seq_len=500)
         mod.compile(loss = {'s':'categorical_crossentropy',
                             'e1':'categorical_crossentropy',
                             'e2':'categorical_crossentropy',
@@ -50,21 +46,21 @@ def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_
                 'dropout': 0.2
             })
 
-            hist = mod.fit(X_train_pad, (y_train,y_train1,y_train2, y_train3, y_train4, y_train5, y_train6), 
+            hist = mod.fit(data_x, data_y, 
                             batch_size=batch_size,
                             epochs=epochs,
-                            validation_data=(X_val_pad,(y_val, y_val1, y_val2, y_val3, y_val4, y_val5, y_val6)),
+                            validation_data=(val_x,val_y),
                             #validation_data=(X_test_pad,(y_test, y_test1, y_test2, y_test3, y_test4, y_test5, y_test6)),
                             callbacks=[tensorboard_callback, hparams_callback])
         else:
-            hist = mod.fit(X_train_pad, (y_train,y_train1,y_train2, y_train3, y_train4, y_train5, y_train6), 
+            hist = mod.fit(data_x, data_y,  
                             batch_size=batch_size,
                             epochs=epochs,
-                            validation_data=(X_val_pad,(y_val, y_val1, y_val2, y_val3, y_val4, y_val5, y_val6)))
+                            validation_data=(val_x,val_y))
         mod.save(saved_model_name)
     
     if model_name == 'mtl_five_sentiments':
-        mod = net().mtl_five_sentiments_model(gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout)
+        mod = net().mtl_five_sentiments_model(vocab_size = vocab_size, embedd_matrix = embedd_matrix, gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout, num_classes1 = 2, embed_num_dims=300, max_seq_len=500)
         mod.compile(loss = {'s':'categorical_crossentropy',
                             'e1':'categorical_crossentropy',
                             'e2':'categorical_crossentropy',
@@ -97,22 +93,22 @@ def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_
                 'dropout': 0.2
             })
 
-            hist = mod.fit(X_train_pad, (y_train,y_train1,y_train2, y_train3, y_train4, y_train5, y_train6), 
+            hist = mod.fit(data_x, data_y, 
                             batch_size=batch_size,
                             epochs=epochs,
-                            validation_data=(X_val_pad,(y_val, y_val1, y_val2, y_val3, y_val4, y_val5, y_val6)),
+                            validation_data=(val_x,val_y),
                             callbacks=[tensorboard_callback, hparams_callback])
         else:
-            hist = mod.fit(X_train_pad, (y_train,y_train1,y_train2, y_train3, y_train4, y_train5, y_train6), 
+            hist = mod.fit(data_x, data_y, 
                             batch_size=batch_size,
                             epochs=epochs,
-                            validation_data=(X_val_pad,(y_val, y_val1, y_val2, y_val3, y_val4, y_val5, y_val6)))
+                            validation_data=(val_x,val_y))
 
         mod.save(saved_model_name)
     
     elif model_name == 'gru_one_task_sentiment':
 
-        mod = net().gru_one_task_sentiment(gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout)
+        mod = net().gru_one_task_sentiment(vocab_size = vocab_size, embedd_matrix = embedd_matrix, gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout, num_classes = 7, embed_num_dims=300, max_seq_len=500)
         mod.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
         if tensorboard == True:
             log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -122,21 +118,21 @@ def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_
                 'num_relu_units': 512,
                 'dropout': 0.2
                 })
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val_pad,y_val),
+                    validation_data=(val_x, val_y),
                     callbacks=[tensorboard_callback, hparams_callback])  
         else:
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val_pad,y_val))  
+                    validation_data=(val_x,val_y))  
         mod.save(saved_model_name)
 
     elif model_name == 'gru_one_task_sentiment_five':
 
-        mod = net().gru_one_task_sentiment_five(gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout)
+        mod = net().gru_one_task_sentiment_five(vocab_size = vocab_size, embedd_matrix = embedd_matrix, gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout, embed_num_dims =300, max_seq_len=500)
         mod.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
         if tensorboard == True:
             log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -146,21 +142,21 @@ def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_
                 'num_relu_units': 512,
                 'dropout': 0.2
                 })
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val_pad,y_val),
+                    validation_data=(val_x,val_y),
                     callbacks=[tensorboard_callback, hparams_callback])  
         else:
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val_pad,y_val))  
+                    validation_data=(val_x,val_y))  
         mod.save(saved_model_name)
 
     elif model_name == 'lstm_one_task_sentiment':
 
-        mod = net().lstm_one_task_sentiment(gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout)
+        mod = net().lstm_one_task_sentiment(vocab_size = vocab_size, embedd_matrix = embedd_matrix, gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout, embed_num_dims = 300, max_seq_len=500)
         mod.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
         if tensorboard == True:
             log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -170,21 +166,21 @@ def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_
                 'num_relu_units': 512,
                 'dropout': 0.2
             })
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val_pad,y_val),
+                    validation_data=(val_x,val_y),
                     callbacks=[tensorboard_callback, hparams_callback])  
         else:
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                  batch_size=batch_size,
                  epochs=epochs,
-                 validation_data=(X_val_pad,y_val))          
+                 validation_data=(val_x,val_y))          
         mod.save(saved_model_name)
         
     elif model_name == 'lstm_one_task_sentiment_five':
 
-        mod = net().lstm_one_task_sentiment_five(gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout)
+        mod = net().lstm_one_task_sentiment_five(vocab_size = vocab_size, embedd_matrix = embedd_matrix, gru_output_size=gru_output_size, dropout=dropout, recurrent_dropout=recurrent_dropout, embed_num_dims = 300, max_seq_len=500)
         mod.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
         if tensorboard == True:
             log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -194,16 +190,16 @@ def train_model(model_name, saved_model_name, tensorboard,  loss_weights, batch_
                 'num_relu_units': 512,
                 'dropout': 0.2
             })
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                     batch_size=batch_size,
                     epochs=epochs,
-                    validation_data=(X_val_pad,y_val),
+                    validation_data=(val_x,val_y),
                     callbacks=[tensorboard_callback, hparams_callback])  
         else:
-            hist = mod.fit(X_train_pad, y_train, 
+            hist = mod.fit(data_x, data_y, 
                  batch_size=batch_size,
                  epochs=epochs,
-                 validation_data=(X_val_pad,y_val))          
+                 validation_data=(val_x,val_y))          
         mod.save(saved_model_name)
         
     
